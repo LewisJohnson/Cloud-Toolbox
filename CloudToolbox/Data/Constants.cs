310bf8a9ba73b48e371be35bf84a7efdfb9e8522
Lewis Johnson <LewisJohnsonDev@gmail.com>
2023-02-04 21:50:36 +0000

    Added shared input component

---
+++ CloudToolbox/Data/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudToolbox.Data
+{
+    public static class Constants
+    {
+        public static string APP_NAME = "Cloud Toolbox";
+        public static string TITLE_PREFIX = $"{APP_NAME} -";
+    }
+}
