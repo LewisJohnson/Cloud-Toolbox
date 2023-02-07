9caf673b18299618ada425bee16708a711d6576b
Lewis Johnson <LewisJohnsonDev@gmail.com>
2023-02-07 21:34:18 +0000

    Moved page routes to dedicated routes class

--- CloudToolbox.Common/Routes/Routes.cs
+++ CloudToolbox.Common/Routes/Routes.cs
@@ -2,46 +2,41 @@
 {
     public static class Routes
     {
-        // Date & Time
-        public const string DURATION_BETWEEN_DAYS = "Toolbox/DateTime/Between-Days";
-        public const string DURATION_BETWEEN_TIMES = "Toolbox/DateTime/Between-Times";
+        // DATE & TIME
+        public const string DATE_TIME_DURATION_BETWEEN_DAYS = "/Toolbox/DateTime/Between-Days";
+        public const string DATE_TIME_DURATION_BETWEEN_TIMES = "/Toolbox/DateTime/Between-Times";
 
-        // Text
-        public const string TO_LOWER_CASE = "Toolbox/Text/To-Lower-Case";
-        public const string TO_UPPER_CASE = "Toolbox/Text/To-Upper-Case";
-        public const string TO_TITLE_CASE = "Toolbox/Text/To-Title-Case";
+        // TEXT
+        public const string TEXT_TO_LOWER_CASE = "/Toolbox/Text/To-Lower-Case";
+        public const string TEXT_TO_UPPER_CASE = "/Toolbox/Text/To-Upper-Case";
+        public const string TEXT_TO_TITLE_CASE = "/Toolbox/Text/To-Title-Case";
 
-        // Travel
-        public const string MPG_CALCULATOR = "Toolbox/Travel/Mpg-Calculator";
-        public const string KML_CALCULATOR = "Toolbox/Travel/Kml-Calculator";
-        public const string REQUIRED_FUEL = "Toolbox/Travel/Required-Fuel";
+        // TRAVEL
+        public const string TRAVEL_MPG_CALCULATOR = "/Toolbox/Travel/Mpg-Calculator";
+        public const string TRAVEL_KML_CALCULATOR = "/Toolbox/Travel/Kml-Calculator";
+        public const string TRAVEL_FUEL_REQUIRED = "/Toolbox/Travel/Fuel-Required";
 
         // UNITS
-        public const string UNITS_KG_TO_STONE = "Toolbox/Units/Kg-to-Stone";
-        public const string UNITS_STONE_TO_KG = "Toolbox/Units/Stone-to-Kg";
-
-        public const string UNITS_MPG_TO_KPH = "Toolbox/Units/Mph-to-Kph";
-        public const string UNITS_KPH_TO_MPG = "Toolbox/Units/Kph-to-Mph";
-
-        public const string UNITS_INCH_TO_CM = "Toolbox/Units/Inch-to-Cm";
-        public const string UNITS_CM_TO_INCH = "Toolbox/Units/Cm-to-Inch";
+        public const string UNITS_KG_TO_STONE = "/Toolbox/Units/Kg-to-Stone";
+        public const string UNITS_STONE_TO_KG = "/Toolbox/Units/Stone-to-Kg";
+        public const string UNITS_MPH_TO_KPH = "/Toolbox/Units/Mph-to-Kph";
+        public const string UNITS_KPH_TO_MPH = "/Toolbox/Units/Kph-to-Mph";
+        public const string UNITS_INCH_TO_CM = "/Toolbox/Units/Inch-to-Cm";
+        public const string UNITS_CM_TO_INCH = "/Toolbox/Units/Cm-to-Inch";
 
         // MATHS
-        public const string MATHS_PERCENTAGE_CALCULATOR = "Toolbox/Maths/Percentage-Calculator";
-        public const string MATHS_MATHS_TIME_TABLES = "Toolbox/Maths/Times-Table";
-        public const string MATHS_TIME_TABLES_GRID = "Toolbox/Maths/Times-Table-grid";
-        public const string MATHS_DIVISION_TABLES = "Toolbox/Maths/Division-Tables";
+        public const string MATHS_PERCENTAGE_CALCULATOR = "/Toolbox/Maths/Percentage-Calculator";
+        public const string MATHS_TIMES_TABLE = "/Toolbox/Maths/Times-Table";
+        public const string MATHS_TIMES_TABLE_GRID = "/Toolbox/Maths/Times-Table-grid";
+        public const string MATHS_DIVISION_TABLE = "/Toolbox/Maths/Division-Table";
 
 
         // DEVELOPER
-
-        public const string DEVELOPER_TO_BASE64 = "Toolbox/Dev/To-Base64";
-        public const string DEVELOPER_FROM_BASE64 = "Toolbox/Dev/From-Base64";
-
-        public const string DEVELOPER_TO_HEX = "Toolbox/Dev/To-Hex";
-        public const string DEVELOPER_FROM_HEX = "Toolbox/Dev/From-Hex";
-
-        public const string DEVELOPER_TO_BINARY = "Toolbox/Dev/To-Binary";
-        public const string DEVELOPER_FROM_BINARY = "Toolbox/Dev/From-Binary";
+        public const string DEVELOPER_TO_BASE64 = "/Toolbox/Dev/To-Base64";
+        public const string DEVELOPER_FROM_BASE64 = "/Toolbox/Dev/From-Base64";
+        public const string DEVELOPER_TO_HEX = "/Toolbox/Dev/To-Hex";
+        public const string DEVELOPER_FROM_HEX = "/Toolbox/Dev/From-Hex";
+        public const string DEVELOPER_TO_BINARY = "/Toolbox/Dev/To-Binary";
+        public const string DEVELOPER_FROM_BINARY = "/Toolbox/Dev/From-Binary";
     }
 }
