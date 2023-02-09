ecf384d42c08e30182a4bac5925ac2981dbc9569
Lewis Johnson <LewisJohnsonDev@gmail.com>
2023-02-09 14:56:53 +0000

    Removed CalculatorsCollection

--- CloudToolbox/Data/Calculators.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CloudToolbox.Data;
-
-public static class Calculators
-{
-	public static List<Calculator> GetCalculators()
-	{
-		return Areas.GetAreas().SelectMany(x => x.Calculators).ToList();
-	}
-}
