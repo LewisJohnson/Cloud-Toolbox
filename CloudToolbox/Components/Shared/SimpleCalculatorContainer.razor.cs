5df844117b9fd459d9d6755f0840c9a3530dab8b
Lewis Johnson <LewisJohnsonDev@gmail.com>
2023-02-16 14:48:20 +0000

    Added breadcrumbs

--- CloudToolbox/Components/Shared/SimpleCalculatorContainer.razor.cs
+++ CloudToolbox/Components/Shared/SimpleCalculatorContainer.razor.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using CloudToolbox.Common.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static CloudToolbox.Components.Shared.Breadcrumb;
 
 namespace CloudToolbox.Components.Shared
 {
@@ -11,6 +14,65 @@ namespace CloudToolbox.Components.Shared
 		public RenderFragment Calculator { get; set; }
 
 		[Parameter]
-		public Common.Data.CalculatorCollection Collection { get; set; }
+		public CalculatorCollection Collection { get; set; }
+
+		public List<BreadcrumbLink> BreadcrumbLinks { get; set; }
+
+		[Inject]
+		public NavigationManager NavigationManager { get; set; }
+
+		private string currentUrl;
+
+		protected override void OnParametersSet()
+		{
+			BreadcrumbLinks = new List<BreadcrumbLink>();
+			currentUrl = NavigationManager.Uri;
+			var myUrl = currentUrl.Replace(NavigationManager.BaseUri, "");
+			BreadcrumbLinks.Add(new BreadcrumbLink
+			{
+				Address = NavigationManager.BaseUri,
+				IsActive = NavigationManager.Uri == NavigationManager.BaseUri,
+				OrderIndex = 1,
+				Title = "Home"
+			});
+
+			var path = myUrl.Split('/');
+			var count = 1;
+
+			foreach (var link in path)
+			{
+				if (string.IsNullOrWhiteSpace(link))
+				{
+					continue;
+				}
+
+				count++;
+				var lastLink = BreadcrumbLinks.Last();
+
+				BreadcrumbLinks.Add(new BreadcrumbLink
+				{
+					Address = $"{lastLink.Address}/{link}",
+					IsActive = link == path.Last(),
+					OrderIndex = count,
+					Title = BreadcrumbTitle(link)
+				});
+			}
+
+			base.OnParametersSet();
+		}
+
+		public static string BreadcrumbTitle(string? val)
+		{
+			if (val == null)
+			{
+				return val;
+			}
+
+			val = string.Join("", val.Select(ch => (char.IsUpper(ch) ? " " : "") + ch));
+			val = val.Replace('-', ' ');
+			val = val.Replace('_', ' ');
+
+			return val;
+		}
 	}
 }
