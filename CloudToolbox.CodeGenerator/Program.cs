3112f2e7f91a6023f30fa43648fb6c872104ad5c
Lewis Johnson <LewisJohnsonDev@gmail.com>
2023-02-16 11:59:26 +0000

    Added css variables

--- CloudToolbox.CodeGenerator/Program.cs
+++ CloudToolbox.CodeGenerator/Program.cs
@@ -2,6 +2,7 @@
 using CloudToolbox.CodeGenerator;
 using CloudToolbox.Common.Enums;
 using CloudToolbox.Common.Enums.Units;
+using CloudToolbox.Services;
 
 static void UnitConverterGenerator<TEnum>() where TEnum : struct, Enum
 {
@@ -21,6 +22,29 @@ static void UnitConverterGenerator<TEnum>() where TEnum : struct, Enum
 }
 
 
+static void CssGenerator()
+{
+	var service = new CalculatorCollectionService();
+
+	Console.WriteLine(":root {");
+	foreach (var col in service.GetCollections())
+	{
+		Console.WriteLine($"--collection-accent-colour-{col.Name}: {col.CssAccentColour};");
+		Console.WriteLine($"--collection-calc-header-colour-{col.Name}: {col.CssCalulatorHeaderBackgroundColour};");
+		Console.WriteLine($"--collection-panel-colour-{col.Name}: {col.CssCalulatorPanelColour};");
+		Console.WriteLine($"--collection-header-colour-{col.Name}: {col.CssHeaderBackgroundColour};");
+	}
+	Console.WriteLine("}");
+	Console.WriteLine("");
+
+	foreach (var col in service.GetCollections())
+	{
+		Console.WriteLine($".card.homepage.{col.Name} {{border: 1px solid var(--collection-accent-colour-{col.Name}); }}");
+		Console.WriteLine($".card.homepage.{col.Name}:hover {{background: 1px solid var(--collection-header-colour-{col.Name}); }}");
+		Console.WriteLine("");
+	}
+}
+
 //RouteGenerator.GenerateAllCalculators();
 //UnitOfXConverterGenerator.GenerateAllCalculators();
 //CalculatorCollectionPropertiesGenerator.GenerateAllCalculatorProps();
@@ -28,5 +52,10 @@ static void UnitConverterGenerator<TEnum>() where TEnum : struct, Enum
 //UnitConverterGenerator<UnitOfLength>();
 //UnitConverterGenerator<UnitOfMass>();
 //UnitConverterGenerator<UnitOfSpeed>();
-UnitConverterGenerator<UnitOfDataTranser>();
-Console.WriteLine(" ");
+//UnitConverterGenerator<UnitOfDataTranser>();
+//Console.WriteLine(" ");
+
+CssGenerator();
+
+
+
